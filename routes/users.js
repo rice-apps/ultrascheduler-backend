85a8174cf839cbf8ae8fddfb28f6b7981c9ff0fe
donglinwang-rice <dw50@rice.edu>
2020-02-01 13:33:48 -0600

    adds dummy functions under user route

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/schedules/:s', function(req, res, next) {
+  res.send(req.params.s);
+});
+router.get('/info', function(req, res, next) {
+  res.send("ok");
 });
 
 module.exports = router;
