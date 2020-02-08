fc6026e8d70cc5b86cd255adf5a697a9c50a3cd6
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-02-08 12:54:45 -0600

    Python script added

--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var querystring = require('querystring');
+const https = require("https");
+
+var options = new URL("https://courses.rice.edu/admweb/!swkscat.cat?format=XML&p_action=query&p_term=201910")
 
 // GET courses
 router.get('/', function (req, res) {
@@ -10,6 +13,16 @@ router.get('/', function (req, res) {
     var ins = req.query.ins;
     res.send(subject);
 });
+
+router.get('/fetch', (req, res) => {
+    console.log("Fetch!");
+    const request = https.request(options, (response) => {
+        console.log("Reached inside");
+        res.on('data', (d) => console.log(d));
+    });
+
+    res.send("Fetched.");
+})
 
 /* GET users listing. */
 // router.get('/', function (req, res, next) {
