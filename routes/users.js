05cc68e8d5212dd202cde17d4b142c33a8335fa6
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-02-22 01:54:30 -0500

    Creation method working with backend

--- routes/users.js
+++ routes/users.js
@@ -10,5 +10,18 @@ router.get('/schedules/:s', function(req, res, next) {
 router.get('/info', function(req, res, next) {
   res.send("ok");
 });
+router.get('/create', (req, res, next) => {
+  Dummy.create({
+    netid: "wsm3",
+    firstName: "Will",
+    lastName: "Mundy",
+    role: "user",
+    idealHour: 6,
+    maxHour: 8,
+    totalHours: 8
+  });
+
+  res.send("Created new user.");
+})
 
 module.exports = router;
