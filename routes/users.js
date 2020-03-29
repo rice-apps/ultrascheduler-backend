586ca66c51d555a9f5eda776b551053055dc98a6
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-03-29 17:08:44 -0400

    User routes added

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
-const Dummy = require("../models/dummyModel").dummy;
+const User = require("../models/usersModel").user;
 
 var express = require('express');
 var router = express.Router();
@@ -7,21 +7,25 @@ var router = express.Router();
 router.get('/schedules/:s', function(req, res, next) {
   res.send(req.params.s);
 });
+
 router.get('/info', function(req, res, next) {
   res.send("ok");
 });
-router.get('/create', (req, res, next) => {
-  Dummy.create({
-    netid: "wsm3",
-    firstName: "Will",
-    lastName: "Mundy",
-    role: "user",
-    idealHour: 6,
-    maxHour: 8,
-    totalHours: 8
-  });
 
-  res.send("Created new user.");
+router.get('/findUser', async (req, res, next) => {
+	let user = await User.findOne({ "netid": req.query.netid });
+	res.json(user);
+})
+
+router.post('/create', async (req, res, next) => {
+    let user = await User.create({
+		netid: req.body.netid,
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		majors: req.body.majors
+    });
+
+  	res.json(user);
 })
 
 module.exports = router;
