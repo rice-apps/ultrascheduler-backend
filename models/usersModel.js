1f8deae2d70024fa366b4bf0eed057f643c57840
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-03-29 18:41:46 -0400

    Added usersModel file

---
+++ models/usersModel.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose')
+    , Schema = mongoose.Schema
+
+require('../db')
+
+const Course = require("../models/coursesModel").course;
+
+var ScheduleSchema = new Schema({
+    term: { type: String },
+    courses: [ {type: Schema.Types.ObjectID, ref: Course } ]
+})
+
+var UserSchema = new Schema({
+    netid: { type: String },
+    firstName: { type: String },
+    lastName: { type: String },
+    majors: [ { type: String, maxlength: 4 } ],
+    schedules: [ ScheduleSchema ],
+})
+
+var User = mongoose.model("users", UserSchema);
+
+exports.user = User;
