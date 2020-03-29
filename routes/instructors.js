b088b9f3dbadc7c131c2cfa0734b983f45af4d5a
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-03-29 03:19:18 -0400

    Added instructor routes

--- routes/instructors.js
+++ routes/instructors.js
@@ -12,4 +12,28 @@ router.post('/create', (req, res, next) => {
     res.send("Created new instructor.");
 })
 
+router.get('/getInstructor', (req, res, next) => {
+    if (!req.query.firstName || !req.query.lastName) {
+        res.send("First and last name of professor is required.");
+        return;
+    }
+
+    let query = {
+        "firstName": req.query.firstName,
+        "lastName": req.query.lastName
+    };
+
+    Instructor.findOne(query)
+    .exec((err, instructor) => {
+        res.json(instructor);
+    });
+})
+
+router.get('/getInstructors', (req, res, next) => {
+    Instructor.find({})
+    .exec((err, instructors) => {
+        res.json(instructors);
+    });
+})
+
 module.exports = router;
